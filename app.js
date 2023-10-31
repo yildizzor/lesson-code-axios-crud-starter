03f72a69656105a9b4bd956df52f399d97ef6eb3
Yildiz Zor Unlu <yildizzor@gmail.com>
2023-10-31 01:52:14 +0100

    done

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.locals.appTitle = `${projectName}`;
 const index = require("./routes/index.routes");
 app.use("/", index);
 
+const movieCharactersRoutes = require("./routes/movie-characters.routes");
+app.use("/", movieCharactersRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
